f931752a12158adbcc50c19ab169f14aabcdd311
akasha95arts <72637422+akasha1arts@users.noreply.github.com>
2022-01-26 22:05:14 -0500

    Coded collectable scripts

--- Assets/Scripts/Collectables/ColorCores.cs
+++ Assets/Scripts/Collectables/ColorCores.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class ColorCores : MonoBehaviour
 {
+    private PlayerCollects playerCollects;
+
+
+
+    void awake()
+    {
+        playerCollects = GetComponent<PlayerCollects>();
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,19 @@ public class ColorCores : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player") // Player collides with core and core will disappear
+        {
+            playerCollects.whiteBlackCores += 1;
+            Destroy(this.gameObject);
+        }
+
+    }
+
+
 }
--- Assets/Scripts/Collectables/PlayerCollects.cs
+++ Assets/Scripts/Collectables/PlayerCollects.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerCollects : MonoBehaviour
 {
+
+    public int whiteBlackCores;
+
+    public TextMeshProUGUI coreAmount;
+    void awake()
+    {
+        whiteBlackCores = 0;
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+      
     }
 
     // Update is called once per frame
     void Update()
     {
+        coreAmount.text = whiteBlackCores.ToString();
         
     }
+
+ 
+
+
+
+
 }
